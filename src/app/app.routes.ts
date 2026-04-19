6fd00e2f72fbaa8db38949202478a355c1067b83
Jonathan Golimlim <golimlim2209@gmail.com>
2026-04-19 16:05:41 +0800

    feat: modify routes and add profile and dashboard page
    - incorporated blog and home into dashboard page as childred

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,31 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Blog } from './pages/blog/blog';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
   {
-    path: "home",
-    component: Home,
+    path: "dashboard",
+    component: Dashboard,
+    children: [
+      {
+        path: "home",
+        component: Home,
+      },
+      {
+        path: "blog",
+        component: Blog,
+      },
+    ]
   },
   {
-    path: "blog",
-    component: Blog,
+    path: "profile",
+    component: Profile
   },
   {
     path: "**",
-    redirectTo: "home",
+    redirectTo: "dashboard",
     pathMatch: "full"
   }
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,11 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { HlmCardImports } from '@spartan-ng/helm/card';
 import { Card } from "../../components/card/card";
-import { Profile } from './components/profile/profile';
 
 @Component({
   selector: 'app-home',
-  imports: [Profile],
+  imports: [],
   templateUrl: './home.html',
   styles: ``,
 })
