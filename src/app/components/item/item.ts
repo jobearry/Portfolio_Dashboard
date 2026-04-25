3603b2bc5b86f5a2664c764d338bed62a40e55b9
Jonathan Golimlim <golimlim2209@gmail.com>
2026-04-25 19:41:46 +0800

    refactor: add item and portfolio component transitions.
    - Added a new file `transitions.ts` for smooth transitions constants using tailwind utils.
    - Adjusted `portfolio.html` to display experiences and projects in a grid layout with scrolling content.
    - Enhanced card components to include loading indicators and additional footer text.

--- src/app/components/item/item.ts
+++ src/app/components/item/item.ts
@@ -4,6 +4,7 @@ import { HlmItemImports } from '@spartan-ng/helm/item';
 import { HlmIcon } from "@spartan-ng/helm/icon";
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideChevronRight, lucideExternalLink } from '@ng-icons/lucide';
+import { TRANSITION_MOVE_UP } from '../../core/styles/transitions';
 
 @Component({
   selector: 'app-item',
@@ -16,7 +17,8 @@ import { lucideChevronRight, lucideExternalLink } from '@ng-icons/lucide';
   styles: ``,
 })
 export class BlogItem {
-  title = input<string>("")
-  description = input<string>("")
-  icon = input<string>('lucideExternalLink')
+  title = input<string>("");
+  description = input<string>("");
+  icon = input<string>('lucideExternalLink');
+
 }
