61bd733fda6b4d1540572d80e0b2f085ed8e793c
Jonathan Golimlim <golimlim2209@gmail.com>
2026-04-29 23:36:59 +0800

    feat: add spartan dialog component and modify sidebar navigation sections

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -4,38 +4,31 @@ import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideLayoutDashboard, lucideUserCircle } from '@ng-icons/lucide';
 import { HlmSidebarImports } from '@spartan-ng/helm/sidebar';
 import { HlmIcon } from '@spartan-ng/helm/icon';
-import { SidebarGroup } from '../../core/constants/sidebar-sections';
-import { RouterLink, RouterModule } from "@angular/router";
+import { RouterModule } from "@angular/router";
+import { SidebarGroupMenu } from '../../core/constants/sidebar-sections';
 @Component({
   selector: 'app-sidebar',
   imports: [HlmSidebarImports, HlmIcon, NgIcon, RouterModule],
   template: `
     <div hlmSidebarWrapper>
       <hlm-sidebar>
-        <div hlmSidebarHeader>
+        <div hlmSidebarHeader class="p-2">
           <ng-icon hlm name="lucideUserCircle" size="lg"></ng-icon>
           <div class="flex flex-col items-start">
             <h1 class="font-bold text-sm">{{ title() }}</h1>
             <p class="text-[.65rem]">{{ subtitle() }}</p>
           </div>
         </div>
-        <div hlmSidebarContent>
+        <div hlmSidebarContent class="p-2">
           @for (item of sidebarContent(); track $index) {
-            <div hlmSidebarGroup>
-              <div hlmSidebarGroupLabel>{{ item.groupLabel }}</div>
-              <div hlmSidebarContent>
-                @for (content of item.groupContent; track $index) {
-                  <ul hlmSidebarMenu>
-                    <li hlmSidebarMenuItem>
-                      <a hlmSidebarMenuButton [routerLink]="[content.url]">
-                        <ng-icon hlm [name]="content.icon" />
-                        <span>{{ content.title }}</span>
-                      </a>
-                    </li>
-                  </ul>
-                }
-              </div>
-            </div>
+            <ul hlmSidebarMenu>
+              <li hlmSidebarMenuItem>
+                <a hlmSidebarMenuButton [routerLink]="[item.url]">
+                  <ng-icon hlm [name]="item.icon" />
+                  <span>{{ item.title }}</span>
+                </a>
+              </li>
+            </ul>
           }
         </div>
         <div hlmSidebarFooter></div>
@@ -50,5 +43,5 @@ import { RouterLink, RouterModule } from "@angular/router";
 export class Sidebar {
   title = input<string>('');
   subtitle = input<string>('Jobearry');
-  sidebarContent = input<SidebarGroup[]>([]);
+  sidebarContent = input<SidebarGroupMenu[]>([]);
 }
