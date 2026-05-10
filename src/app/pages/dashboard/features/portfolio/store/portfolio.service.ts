c1e64d40be2b46d650ba09d6d528aecc3f2f694d
Jonathan Golimlim <golimlim2209@gmail.com>
2026-05-10 19:04:10 +0800

    feat: enhance portfolio component with skills section and update project dialog stylings

--- src/app/pages/dashboard/features/portfolio/store/portfolio.service.ts
+++ src/app/pages/dashboard/features/portfolio/store/portfolio.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { JGExperience } from "../core/models/experience";
 import { JGProject } from "../core/models/project";
 import { PortfolioSignalService } from "./portfolio-base.service";
+import { JGTechStackDTO } from "../core/models/techstack";
 
 @Injectable({ providedIn: 'root' })
 export class ProjectSignalService extends PortfolioSignalService<JGProject> {}
@@ -10,4 +11,4 @@ export class ProjectSignalService extends PortfolioSignalService<JGProject> {}
 export class ExperienceSignalService extends PortfolioSignalService<JGExperience> {}
 
 @Injectable({ providedIn: 'root' })
-export class SkillsService extends PortfolioSignalService<JGExperience> {}
+export class SkillsService extends PortfolioSignalService<JGTechStackDTO> {}
--- src/app/pages/dashboard/features/portfolio/ui/portfolio.ts
+++ src/app/pages/dashboard/features/portfolio/ui/portfolio.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Card } from '../../../../../components/card/card';
 import { PORTFOLIO_CARD_FOOTER_STYLES, TRANSITION_MOVE_UP } from '../core/styles/portfolio.styles';
 import { JGExperience } from '../core/models/experience';
-import { ExperienceSignalService, ProjectSignalService } from '../store/portfolio.service';
+import { ExperienceSignalService, ProjectSignalService, SkillsService } from '../store/portfolio.service';
 import { SkeletonItem } from "../../../../../components/skeletons/item/skeleton-item";
 import { HlmH3 } from "@spartan-ng/helm/typography";
 import { HlmIcon } from "@spartan-ng/helm/icon";
@@ -13,24 +13,27 @@ import { getContent } from '../core/portfolio.util';
 import { ProjectDialog } from '../components/dialog/project-dialog';
 import { JGProject } from '../core/models/project';
 import { Maintenance } from "../../../../../components/maintenance.ts/maintenance";
+import { JGTechStackDTO } from '../core/models/techstack';
 
 @Component({
   selector: 'app-resume',
-  imports: [Card, SkeletonItem, HlmIcon, NgIcon, HlmH3, Maintenance],
+  imports: [Card, SkeletonItem, HlmIcon, NgIcon, HlmH3],
   templateUrl: './portfolio.html',
   providers: [provideIcons({ lucideInfo })],
   styles: ``,
 })
 export class Portfolio implements OnInit {
   _hlmDialogService = inject(HlmDialogService);
-  _projectService = inject(ProjectSignalService)
-  _expService = inject(ExperienceSignalService)
+  _projectService = inject(ProjectSignalService);
+  _expService = inject(ExperienceSignalService);
+  _skillsService = inject(SkillsService);
   portfolioCardFooterStyle = PORTFOLIO_CARD_FOOTER_STYLES
   transitionMoveUp = TRANSITION_MOVE_UP
 
   ngOnInit(): void {
 
     getContent<JGExperience>('v1/Experiences', this._expService)
+    getContent<JGTechStackDTO>('v1/TechStack', this._skillsService)
   }
 
   async openDialog(experience: JGExperience){
