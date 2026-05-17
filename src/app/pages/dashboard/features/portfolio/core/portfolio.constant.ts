915afbd4fa9b025c556d874a80f330933f55c862
Jonathan Golimlim <golimlim2209@gmail.com>
2026-05-17 17:19:37 +0800

    feat(portfolio): integrate portfolio icons and controls into the portfolio component

---
+++ src/app/pages/dashboard/features/portfolio/core/portfolio.constant.ts
@@ -0,0 +1,14 @@
+import { lucideDrill, lucideInfo, lucideLibraryBig, lucideToolCase } from "@ng-icons/lucide";
+
+export const PORTFOLIO_ICONS: Record<string, string> = {
+  lucideInfo,
+  lucideToolCase,
+  lucideLibraryBig,
+  lucideDrill
+};
+
+export const PORTFOLIO_CONTROLS: Record<string, string>[] = [
+  {"sectionName": "Projects", "iconName": "lucideDrill"},
+  {"sectionName": "Skills", "iconName": "lucideToolCase"},
+  {"sectionName": "Experiences", "iconName": "lucideLibraryBig"},
+]
--- src/app/pages/dashboard/features/portfolio/ui/portfolio.ts
+++ src/app/pages/dashboard/features/portfolio/ui/portfolio.ts
@@ -3,20 +3,18 @@ import { PORTFOLIO_CARD_FOOTER_STYLES, TRANSITION_MOVE_UP } from '../core/styles
 import { JGExperience } from '../core/models/experience';
 import { ExperienceSignalService, ProjectSignalService, SkillsService } from '../store/portfolio.service';
 import { HlmH3 } from "@spartan-ng/helm/typography";
-import { HlmIcon } from "@spartan-ng/helm/icon";
-import { NgIcon, provideIcons } from "@ng-icons/core";
-import { lucideInfo, lucideLibraryBig, lucideToolCase } from '@ng-icons/lucide';
+import { NgIcon } from "@ng-icons/core";
 import { HlmDialogService } from '../../../../../../../libs/ui/dialog/src/lib/hlm-dialog.service';
 import { getContent } from '../core/portfolio.util';
 import { ProjectDialog } from '../components/dialog/project-dialog';
 import { JGProject } from '../core/models/project';
 import { JGTechStackDTO } from '../core/models/techstack';
+import { PORTFOLIO_CONTROLS } from '../core/portfolio.constant';
 
 @Component({
   selector: 'app-resume',
   imports: [HlmH3, NgIcon],
   templateUrl: './portfolio.html',
-  providers: [provideIcons({ lucideInfo, lucideToolCase, lucideLibraryBig })],
   styles: ``,
 })
 export class Portfolio implements OnInit {
@@ -24,8 +22,11 @@ export class Portfolio implements OnInit {
   _projectService = inject(ProjectSignalService);
   _expService = inject(ExperienceSignalService);
   _skillsService = inject(SkillsService);
+
   portfolioCardFooterStyle = PORTFOLIO_CARD_FOOTER_STYLES
   transitionMoveUp = TRANSITION_MOVE_UP
+
+  controls = PORTFOLIO_CONTROLS
 
   ngOnInit(): void {
     getContent<JGExperience>('v1/portfolio/Experiences', this._expService)
